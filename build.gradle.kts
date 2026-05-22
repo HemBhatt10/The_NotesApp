f1e7864216e5f21945e75a342b33615847401b9b
Hem Bhatt <bhatthem184@gmail.com>
2026-05-22 10:14:49 +0530

    complete ui which have home page,add page,edit page and delete page

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+
+    dependencies {
+        val navVersion = "2.7.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
+
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
